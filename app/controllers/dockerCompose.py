ec61721a9c71c7509e5882dff5096baef93b381b
Rajeev Singh <rsinghh68@gmail.com>
2024-03-08 14:50:50 +0530

    added docker compose down statement and removed flock

--- app/controllers/dockerCompose.py
+++ app/controllers/dockerCompose.py
@@ -2,24 +2,23 @@ import subprocess
 import flock 
 
 def update_docker_compose(commit_hash_after, commit_hash_before, docker_compose_path):
-    try:
+
         # Read the current Docker Compose file content
         with open(docker_compose_path, 'r') as f:
-            docker_compose_content = f.read()
+            docker_compose_content = f.read()   
 
         # Replace the commit hash_before with commit_hash_after
         updated_content = docker_compose_content.replace(commit_hash_before, commit_hash_after)
 
         # Write the updated content back to the Docker Compose file
-        with flock.Flock(docker_compose_path, flock.LOCK_EX):
-            with open(docker_compose_path, 'w') as f:
-                f.write(updated_content)
-    except Exception as e:
-        print(f'Error updating Docker Compose file: {e}')
+        with open(docker_compose_path, 'w') as f:
+            f.write(updated_content)
+
         
 def restart_docker_compose(docker_compose_path: str) -> str:
     try:
         # Restart the Docker Compose services and capture the logs
+        completed_process = subprocess.run(['docker', 'compose', '-f', docker_compose_path, 'down'], capture_output=True, text=True, check=True)
         completed_process = subprocess.run(['docker', 'compose', '-f', docker_compose_path, 'up', '-d'], capture_output=True, text=True, check=True)
 
         # Check if the Docker Compose services restarted properly
